39bd6bb8fa7d81bde3949689390bee267d668a47
protarie125 <proarie@gmail.com>
2022-02-20 08:44:07 +0900

    초기 커밋.

---
+++ MainApp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+
+namespace BOJ_1247
+{
+    internal static class MainApp
+    {
+        private static void Main(string[] args)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                var nstr = Console.ReadLine();
+                var n = int.Parse(nstr);
+
+                var sum = default(BigInteger);
+                for (int j = 0; j < n; j++)
+                {
+                    var line = Console.ReadLine();
+                    var val = BigInteger.Parse(line);
+                    sum += val;
+                }
+
+                if (sum < 0)
+                {
+                    Console.WriteLine('-');
+                }
+                else if (0 < sum)
+                {
+                    Console.WriteLine('+');
+                }
+                else
+                {
+                    Console.WriteLine('0');
+                }
+            }
+        }
+    }
+}
